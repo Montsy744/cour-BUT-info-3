e43734b85e28a2b81f0db34bd9ba683688419fc5
Hugo Straseele <hugo.straseele.etu@univ-lille.fr>
2026-09-08 15:36:43 +0200

    1.2 Algorithme itératif

--- Qualité d'algo/tp2/Fibonacci.java
+++ Qualité d'algo/tp2/Fibonacci.java
@@ -12,6 +12,26 @@ public class Fibonacci {
         return fibo_rec_naif(n - 1) + fibo_rec_naif(n - 2);
     }
 
+
+    public static final int fibo_iter(final int n) {
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+
+        int precedent2 = 0;
+        int precedent1 = 1;
+        int courant = 0;
+
+        for (int i = 2; i <= n; i++) {
+            courant = precedent1 + precedent2;
+            compteurAdditions++;
+
+            precedent2 = precedent1;
+            precedent1 = courant;
+        }
+
+        return courant;
+    }
+
     public static void main(final String[] args) {
         final int n = 5;
         compteurAdditions = 0; 
