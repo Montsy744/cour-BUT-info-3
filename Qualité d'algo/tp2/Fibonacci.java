9bd898831fa80b2da3d79240d45cc7d3c813f5bc
Hugo Straseele <hugo.straseele.etu@univ-lille.fr>
2026-09-08 15:32:44 +0200

    1.1 Algorithme récursif naïf

--- Qualité d'algo/tp2/Fibonacci.java
+++ Qualité d'algo/tp2/Fibonacci.java
@@ -1,3 +1,23 @@
 public class Fibonacci {
-    
+    public static int compteurAdditions = 0;
+
+    public static final int fibo_rec_naif(final int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        compteurAdditions++; 
+        return fibo_rec_naif(n - 1) + fibo_rec_naif(n - 2);
+    }
+
+    public static void main(final String[] args) {
+        final int n = 5;
+        compteurAdditions = 0; 
+        final int resultat = fibo_rec_naif(n);
+        
+        System.out.println("Fibo(" + n + ") = " + resultat);
+        System.out.println("Nombre d'additions effectuees : " + compteurAdditions);
+    }
 }
