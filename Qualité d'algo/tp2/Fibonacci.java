8b8df0e77869a6fa9d5f13aecae11cba8071ad32
Hugo Straseele <hugo.straseele.etu@univ-lille.fr>
2026-09-08 15:43:37 +0200

    1.3 Algorithme récursif efficace

--- Qualité d'algo/tp2/Fibonacci.java
+++ Qualité d'algo/tp2/Fibonacci.java
@@ -12,7 +12,6 @@ public class Fibonacci {
         return fibo_rec_naif(n - 1) + fibo_rec_naif(n - 2);
     }
 
-
     public static final int fibo_iter(final int n) {
         if (n <= 0) return 0;
         if (n == 1) return 1;
@@ -32,12 +31,47 @@ public class Fibonacci {
         return courant;
     }
 
+
+    public static final int fibo_rec(final int n) {
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        return fibo_rec_aide(n, 2, 0, 1);
+    }
+
+   
+    private static int fibo_rec_aide(final int n, final int i, final int precedent2, final int precedent1) {
+        final int courant = precedent1 + precedent2;
+        compteurAdditions++;
+
+
+        if (i == n) {
+            return courant;
+        }
+
+        return fibo_rec_aide(n, i + 1, precedent1, courant);
+    }
+
     public static void main(final String[] args) {
-        final int n = 5;
+        final int n = 10; 
+
         compteurAdditions = 0; 
-        final int resultat = fibo_rec_naif(n);
+        int resNaif = fibo_rec_naif(n);
+        int addNaif = compteurAdditions;
+
+        compteurAdditions = 0; 
+        int resIter = fibo_iter(n);
+        int addIter = compteurAdditions;
+
+        compteurAdditions = 0; 
+        int resRec = fibo_rec(n);
+        int addRec = compteurAdditions;
         
-        System.out.println("Fibo(" + n + ") = " + resultat);
-        System.out.println("Nombre d'additions effectuees : " + compteurAdditions);
+        System.out.println("--- Résultats pour n = " + n + " ---");
+        System.out.println("Fibo Naïf     : Valeur = " + resNaif + " | Additions = " + addNaif);
+        System.out.println("Fibo Itératif : Valeur = " + resIter + " | Additions = " + addIter);
+        System.out.println("Fibo Récur.   : Valeur = " + resRec  + " | Additions = " + addRec);
+        
+        System.out.println("\n-> Vérification : Les additions itératives et récursives sont-elles identiques ? " 
+                           + (addIter == addRec ? "OUI" : "NON"));
     }
 }
